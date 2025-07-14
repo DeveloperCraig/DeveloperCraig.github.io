2d6ca1106edea47dfaeab1852443bf8700d3ca33
Craig Bennion <bennioncraig@gmail.com>
2025-07-14 21:55:47 +0100

    Fixed issues with webpack not working correctly.

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 // Add your JavaScript code below
 console.log('Webpack is working!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
+
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  mode: 'development', // or 'production'
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
