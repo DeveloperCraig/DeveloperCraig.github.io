6df81c93e4c22340c3b532f29e45e8a6f86d7529
Craig (Windows) <bennioncraig@gmail.com>
2025-07-16 23:04:06 +0100

    Made a few tweeks to the configeration file

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    clean: true,
   },
   mode: 'development', // or 'production'
   module: {
